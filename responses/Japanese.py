908d7323f1b591d010cd5ba6aca6fe2e1de7e410
Frances <77402420+Desassossego@users.noreply.github.com>
2024-11-27 21:13:16 +0000

    Create Japanese.py

---
+++ responses/Japanese.py
@@ -0,0 +1,10 @@
+arr_japanese = [
+    "ドラマー",
+    "ichi, ni, san, shi, go, roku, nana, hachi, kyu, jyuu",
+    "Arigato, Renato-kun",
+    "Ā, akogare no pokemon masutā ni",
+    "Konnichiwa",
+    "Sayōnara",
+
+    
+]
